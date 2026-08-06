ffca7834a6854869ff5579ae9a52fccfb19f4e97
Edilio Junior <443963@unoesc.lan>
2026-08-05 22:17:51 -0300

    Aula02

---
+++ Att1/palavras/Program.cs
@@ -0,0 +1,29 @@
+﻿string firstname = "Edilio"; // Corrigido de firtname para firstname
+string lastname = "Junior";
+
+string note = lastname.ToUpper() + " " + firstname;
+
+
+string initials = lastname[0].ToString() + firstname[0].ToString(); 
+
+Console.WriteLine("" + initials); 
+
+// Formatação de string
+
+string texto = string.Format(
+    "{0} {1} nascido em {2}", 
+    firstname, 
+    lastname,
+    "2005"
+);
+
+Console.WriteLine(texto); 
+
+// C# é um linguagem filha do C++
+// Totalmente Orientada a Objetos
+// Portanto , tudo dentro do C é descendente
+// do tipo Object
+
+int age = 20;
+object ageBoxing = age;
+int ageUnboxing = (int) ageBoxing;
