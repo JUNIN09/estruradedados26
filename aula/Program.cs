e8aec24b8342d73651915b43e772d8bb9ee29934
Edilio Junior <105878502+JUNIN09@users.noreply.github.com>
2026-09-23 21:13:43 -0300

    Add files via upload

---
+++ aula/Program.cs
@@ -0,0 +1,20 @@
+﻿﻿using System.Globalization;
+
+string[] months = new string[12];
+
+for(int i = 1; i <= 12; i++)
+{
+    DateTime firstDay = new DateTime(DateTime.Now.Year, i, 1);
+
+    DateTime lastDayMonthBefore = firstDay.AddDays(-1);
+
+    string monthName = firstDay.ToString("MMMM", CultureInfo.CreateSpecificCulture("pt-br"));
+
+    months[i - 1] = monthName;
+}
+
+foreach(string monthName in months)
+{
+    Console.WriteLine($"---> {monthName}");
+};
+
